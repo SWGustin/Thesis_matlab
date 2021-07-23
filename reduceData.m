e57e9476c1d9fbbf5fb96e4cb607bf3998db6d2e
Sam Gustin <samwgustin@gmail.com>
2021-07-22 23:50:19 -0700

    close to working, generates figure just not quite right yet

---
+++ reduceData.m
@@ -0,0 +1,9 @@
+function reduced = reduceData(step,data)
+if ndims(data) == 3
+    reduced = data(1:step:end,:,1:step:end);
+elseif ndims(data) == 2
+    reduced = data(:,1:step:end);
+elseif ndims(data) == 1
+    reduced = data(1:step:end);
+end
+            
